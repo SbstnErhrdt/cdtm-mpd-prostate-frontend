7f74929f2aab044144efe3c4a0b79b55a46668a0
Sebastian Erhardt <sebastian@ex-studios.net>
2018-12-17 22:15:25 +0100

    + login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../services/api.service';
+import {DateService} from '../services/date.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +12,26 @@ export class LoginComponent implements OnInit {
 
   form = {};
 
-  constructor() {
+  formdata = new FormGroup({
+    username: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  constructor(private api: ApiService, private dateSerive: DateService) {
   }
 
   ngOnInit() {
+  }
+
+  login(data) {
+    this.api.createData('users/login', data).subscribe(
+      res => {
+
+      },
+      err => {
+
+      },
+    )
   }
 
 }
